677577fc4fa6ec3ac301b80ecd9b5607ab0fc63f
anton.belogurov@fmecgroup.com <anton.belogurov@fmecgroup.com>
2021-11-23 10:19:05 +0300

    new

--- project/app.js
+++ project/app.js
@@ -2,12 +2,31 @@
 let express = require('express'),
     app = express()
 
-app.use(express.static('assets'))
 
+let mysql = require('mysql')
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'noda'
+})
+
+app.use(express.static('assets'))
+app.set('view engine', 'pug')
 app.listen(3000, () => {
     console.log('node work');
 })
 
 app.get('/', (req, res) => {
-    res.render('index.html')
+    con.query('SELECT * FROM goods', (err, result) => {
+
+        let goods = {}
+        for (let i = 0; i < result.length; i++) {
+            goods[result[i]['id']] = result[i]
+        }
+        console.log(goods);
+        res.render('main', {
+            goods: JSON.parse(JSON.stringify(goods))
+        })
+    })
 })

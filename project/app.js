73f61929e5f159ddafba9a119971d40683b569b7
anton.belogurov@fmecgroup.com <anton.belogurov@fmecgroup.com>
2021-11-23 18:01:47 +0300

    lesson 9

--- project/app.js
+++ project/app.js
@@ -19,14 +19,37 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     con.query('SELECT * FROM goods', (err, result) => {
-
         let goods = {}
         for (let i = 0; i < result.length; i++) {
             goods[result[i]['id']] = result[i]
         }
-        console.log(goods);
         res.render('main', {
             goods: JSON.parse(JSON.stringify(goods))
+        })
+    })
+})
+
+app.get('/cat', (req, res) => {
+    let catId = req.query.id
+    let cat = new Promise((resolve, reject) => {
+        con.query('SELECT * FROM category WHERE id =' + catId, (err, result) => {
+            if (err) reject(err);
+            // res.render('cat', {})
+            resolve(result)
+        })
+    })
+
+    let goods = new Promise((resolve, reject) => {
+        con.query('SELECT * FROM goods WHERE category =' + catId, (err, result) => {
+            if (err) reject(err)
+            resolve(result)
+        })
+    })
+
+    Promise.all([cat, goods]).then(function (value) {
+        res.render('cat', {
+            cat: JSON.parse(JSON.stringify(value[0])),
+            goods: JSON.parse(JSON.stringify(value[1]))
         })
     })
 })
